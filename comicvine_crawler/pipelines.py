1839b3c7d393f24e1588dcfe7ac4eb27783e3741
artem.vasin <artem.vasin>
2020-03-24 06:21:49 +0300

    Decrease mongodb collection life to one item

--- comicvine_crawler/pipelines.py
+++ comicvine_crawler/pipelines.py
@@ -13,17 +13,14 @@ class MongoPipeline(object):
             mongo_url=crawler.settings.get('MONGO_URL')
         )
 
-    def open_spider(self, spider):
+    def process_item(self, item, spider):
         self._mongo_client = Connect.get_connection(self._mongo_url)
         self._mongo_collection = self._mongo_client.get_default_database()[spider.name]
-
-    def close_spider(self, spider):
-        self._mongo_client.close()
-
-    def process_item(self, item, spider):
         if not item.get("skip"):
             self._mongo_collection.replace_one({"id": item["id"]}, dict(item), upsert=True)
+            self._mongo_client.close()
             return {"id": item["id"], "api_detail_url": item["api_detail_url"], "crawl_date": item["crawl_date"],
                     "name": item.get("name")}
         else:
+            self._mongo_client.close()
             return item
